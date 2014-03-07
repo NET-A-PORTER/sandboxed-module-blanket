f8f28434eb42869b528aba7dbbf2b3ae022cb77d
Lovell Fuller <Lovell.Fuller@net-a-porter.com>
2014-03-07 10:27:44 +0000

    Add test to verify blanket.js instrumentation is missing without this module

--- test/integration.js
+++ test/integration.js
@@ -6,12 +6,19 @@ var assert = require('assert'),
     pattern: moduleUnderTest
   });
 
-var fixture = SandboxedModule.load('./' + moduleUnderTest, {
+// Verify sandboxed-module does not provide blanket code coverage instrumentation on its own
+var fixtureWithoutBlanket = SandboxedModule.load('./' + moduleUnderTest).exports;
+var instrumentedSourceWithoutBlanket = fixtureWithoutBlanket.answer.toString();
+
+assert(instrumentedSourceWithoutBlanket.indexOf(global._blanket.getCovVar()) === -1);
+assert(instrumentedSourceWithoutBlanket.indexOf(moduleUnderTest) === -1);
+
+// Verify sandboxed-module + sandboxed-module-blanket provides blanket code coverage instrumentation
+var fixtureWithBlanket = SandboxedModule.load('./' + moduleUnderTest, {
   sourceTransformers: SandboxedModuleBlanket
 }).exports;
+var instrumentedSourceWithBlanket = fixtureWithBlanket.answer.toString();
 
-var instrumentedSource = fixture.answer.toString();
-
-assert(instrumentedSource.indexOf(global._blanket.getCovVar()) !== -1);
-assert(instrumentedSource.indexOf(moduleUnderTest) !== -1);
+assert(instrumentedSourceWithBlanket.indexOf(global._blanket.getCovVar()) !== -1);
+assert(instrumentedSourceWithBlanket.indexOf(moduleUnderTest) !== -1);
 
